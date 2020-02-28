0e9b47d95a38dca7de9c1620e62149f72d7bc257
Evan Pearson <evan.pearson@thehutgroup.com>
2020-02-28 23:46:34 +0000

    2020 update, faster

--- main.py
+++ main.py
@@ -1,51 +1,29 @@
-from copy import deepcopy
+import sys
 
-def valid(Word, letters):
-  
-  """
-  This function determines if a word can be constructed using the word wheel.
-  
-  The function which takes three inputs:
-  -- A word
-  -- A list of the letters on the outer word wheel, where letter one is the middle letter of the WW
-  
-  The function outputs:
-  -- True if the word can be constructed using the word wheel
-  -- False if it cannot.
-  """
-  word = Word.lower()
-  if len(word) < 3 or len(word) > 9: return False
-  if letters[0] in word:
-    x = 0
-    for letter in word:
-      x+=1
-      if letter in letters:
-        letters.remove(letter)
-        if x == len(word):
-          return True
-      else: return False
-  else: return False
+def valid(word, centre_letter, outer_letters):
+  word = word.lower()
+  word_length = len(word)
+  all_valid_letters = [centre_letter] + outer_letters
+  if not 3 < word_length <= len(all_valid_letters): 
+    return False
+  if centre_letter not in word:
+    return False
+  for letter in word:
+    if letter not in all_valid_letters:
+      return False
+    all_valid_letters.remove(letter)
+  return True
 
-ml = input("Which letter is in the middle of the word wheel?")
-while(len(ml) != 1 or not ml.isalpha()):
-  ml = input("Which letter is in the middle of the word wheel? Please input a single letter!")
-ml = ml.lower()
+def main(letters):
+  centre_letter = letters[0]
+  outer_letters = [char for char in letters[1:]]
+  with open("dictionary.txt") as file:
+    formatted_words = [word.strip().lower() for word in file.readlines()]
+    valid_words = [word for word in formatted_words if valid(word, centre_letter, outer_letters)]
+  print(valid_words)
 
-ol = [ml]
-for _ in range(8):
-  l = input("Please input the remaining letters, one by one:")
-  while(len(l) != 1 or not l.isalpha()):
-    l = input("Please input the remaining letters, one by one:")
-  ol.append(l.lower())
-print(ol)
-
-validWords = []
-
-with open("dictionary.txt", mode="rt", encoding="utf8") as dic:
-  lst = dic.readlines()
-  for EL in lst:
-    if (valid(EL[:-1], deepcopy(ol))):
-      print(EL)
-      validWords.append(EL[:-1])
-
-print(validWords)
+if __name__ == '__main__':
+  if len(sys.argv) != 2:
+    print('Use one argument. A string containing all letters in the word wheel - beginning with the center letter.')
+    exit()
+  main(sys.argv[1])
